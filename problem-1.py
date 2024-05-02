94cc971bd31a81aae2768005f9adb38388071fb9
Koheli Akter <koheliakter344@gmail.com>
2024-05-02 21:59:17 +0600

    first problem solv

--- problem-1.py
+++ problem-1.py
@@ -4,3 +4,12 @@ If  is odd, print Weird
 If  is even and in the inclusive range of  to , print Not Weird
 If  is even and in the inclusive range of  to , print Weird
 If  is even and greater than , print Not Weird"""
+
+
+n = int(input())
+
+if (n%2 != 0) or (6 <= n and n <= 20):
+    print(" Weird")
+
+else:
+    print (" Not Weird")    
