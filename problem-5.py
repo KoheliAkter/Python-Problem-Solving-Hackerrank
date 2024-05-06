6d217d94912801f99950ef6f2a2816cf5dced023
Koheli Akter <koheliakter344@gmail.com>
2024-05-06 22:51:26 +0600

    problem 5 done

--- problem-5.py
+++ problem-5.py
@@ -3,19 +3,13 @@
 def is_leap(year):
     leap = False
 
-    if year%400 == 0 :
-        return True
-    
-    elif year%100 == 0 :
-        return leap
-    
-    elif year%4 == 0 :
-        return True
- 
-    else:   
-       return leap
+    if (year%4 == 0 and year%100 != 0 ) or (year%400 == 0):
+       leap=True
 
-#is_leap (year = int(input()))
+    else:
+       leap=False   
+         
+    return leap
+
 year = int(input())
-is_leap (year)
 print(is_leap(year))
